d7d714961c49a7c861d8d8da85a5e4d88273ff6d
Mikhail Oleinik <ganddalf@ya.ru>
2025-03-18 09:57:34 +0300

    add virt-manager

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -1,3 +1,10 @@
 {
+  # Docker
   virtualisation.docker.enable = true;
+
+  # Virt-manager
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = [ "ganddalf" ];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 }
